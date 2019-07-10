e1f36338e6f546c2a19bf8fc93477d04ca92bdec
Pily <paila@live.com.mx>
2019-07-10 00:29:18 -0500

    created config with dafault.json and database connect

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 
 app.get("/", (req, res) => res.send("API Running"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// Connect Data Base
+connectDB();
